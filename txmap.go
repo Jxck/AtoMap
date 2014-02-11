78a296b11873ab9936d660c9e0eddd936d5dd76f
Jxck <block.rxckin.beats@gmail.com>
2014-02-12 00:16:29 +0900

    up

--- txmap.go
+++ txmap.go
@@ -1,112 +1,91 @@
-package main
+package txmap
 
 import (
 	"log"
-	"runtime"
 )
-
-var debug func(a ...interface{})
 
 func init() {
 	log.SetFlags(log.Lshortfile)
-	debug = func(a ...interface{}) {
-		log.Printf("%+v", a)
-	}
-	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 type RequestType int
 
 const (
-	SET RequestType = iota
-	GET
-	BEGINTX
-	ENDTX
+	Set RequestType = iota
+	Get
+	BeginTx
+	EndTx
 )
 
-type request struct {
+type Request struct {
 	requestType RequestType
 	key         int
 	value       int
-	result      chan int
-	tx          chan request
+	ret         chan int
+	tx          chan Request
 }
 
 type TxMap struct {
-	m       map[int]int
-	request chan request
+	req chan Request      // メインループ
+	tx  chan chan Request // tx に入った時の退避用
 }
 
 func NewTxMap() *TxMap {
-	txm := &TxMap{
-		m:       make(map[int]int),
-		request: make(chan request),
-	}
-	go txm.Handle(txm.request)
+	txm := new(TxMap)
+	txm.req = make(chan Request)
+	txm.tx = make(chan chan Request, 1)
+	go runMap(txm.req)
 	return txm
 }
 
-func (txm *TxMap) Handle(requestChan chan request) {
+func runMap(r chan Request) {
+	m := make(map[int]int)
+	HandleRequests(m, r)
+}
+
+func HandleRequests(m map[int]int, r chan Request) {
 	for {
-		req := <-requestChan
+		req := <-r
 		switch req.requestType {
-		case GET:
-			req.result <- txm.m[req.key]
-		case SET:
-			old := txm.m[req.key]
-			txm.m[req.key] = req.value
-			req.result <- old
-		case BEGINTX:
-			txm.Handle(req.tx)
-		case ENDTX:
+		case Get:
+			req.ret <- m[req.key]
+		case Set:
+			m[req.key] = req.value
+		case BeginTx:
+			HandleRequests(m, req.tx)
+		case EndTx:
 			return
 		}
 	}
 }
 
-func (txm *TxMap) Set(key int, value int) int {
-	result := make(chan int)
-	request := request{
-		requestType: SET,
-		key:         key,
-		value:       value,
-		result:      result,
-	}
-	txm.request <- request // lock
-	return <-result        // unlock
+func (txm *TxMap) Set(key int, value int) {
+	txm.req <- Request{Set, key, value, nil, nil}
 }
 
 func (txm *TxMap) Get(key int) int {
 	result := make(chan int)
-	request := request{
-		requestType: GET,
-		key:         key,
-		result:      result,
-	}
-	txm.request <- request // lock
-	return <-result        // unlock
+	txm.req <- Request{Get, key, 0, result, nil}
+	return <-result
 }
 
 func (txm *TxMap) BeginTx() {
-	tx := make(chan request)
-	request := request{
-		requestType: BEGINTX,
-		tx:          tx,
-	}
-	txm.request <- request
+	tmp := txm.req
+	txm.tx <- tmp
+	txm.req = make(chan Request)
+	tmp <- Request{BeginTx, 0, 0, nil, txm.req}
 }
 
 func (txm *TxMap) EndTx() {
-	request := request{
-		requestType: ENDTX,
-	}
-	txm.request <- request
+	txm.req <- Request{EndTx, 0, 0, nil, nil}
+	txm.req = <-txm.tx
 }
 
-func main() {
-	txm := NewTxMap()
-	txm.BeginTx()
-	log.Println(txm.Set(1, 1))
-	log.Println(txm.Get(1))
-	txm.EndTx()
-}
+// func main() {
+// 	r := make(chan Request)
+// 	go runMap(r)
+// 	tx := beginTx(r)
+// 	set(tx, 1, "hoge")
+// 	log.Println(get(tx, 1))
+// 	endTx(tx)
+// }
