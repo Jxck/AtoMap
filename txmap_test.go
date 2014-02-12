1865f7247aaadb99881af1fce0b15dd0dfe648ec
Jxck <block.rxckin.beats@gmail.com>
2014-02-12 15:19:28 +0900

    add test

--- txmap_test.go
+++ txmap_test.go
@@ -13,7 +13,7 @@ func init() {
 func TestAtoMap(t *testing.T) {
 	var wg sync.WaitGroup
 	atoMap := NewAtoMap()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
@@ -34,4 +34,46 @@ func TestAtoMap(t *testing.T) {
 	}
 	wg.Wait()
 	t.Log(atoMap.Get(0))
+}
+
+func TestConcurrent(t *testing.T) {
+	m1 := NewAtoMap()
+	m2 := NewAtoMap()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			mm1 := m1.Lock()
+			mm2 := m2.Lock()
+
+			j := mm1.Get(0)
+			j = j + 1
+			mm1.Set(0, j)
+
+			k := mm2.Get(0)
+			k = k + 1
+			mm2.Set(0, k)
+
+			actual := mm1.Get(0)
+			expected := j
+
+			actual = mm2.Get(0)
+			expected = k
+
+			mm1.Unlock()
+			mm2.Unlock()
+
+			if actual != expected {
+				t.Errorf("\ngot  %v\nwant %v", actual, expected)
+			}
+
+			if actual != expected {
+				t.Errorf("\ngot  %v\nwant %v", actual, expected)
+			}
+		}()
+	}
+
 }
